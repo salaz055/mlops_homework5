ad0c5b84776e9196de30170abf26b79234bd0845
John Salazar <101416331+salaz055@users.noreply.github.com>
2025-05-13 21:21:17 -0500

    Update retriever.py

--- src/retriever/retriever.py
+++ src/retriever/retriever.py
@@ -1,3 +1,21 @@
-def get_similar_responses(question: str) -> list:
+import pickle
+import pandas as pd
+import os
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from src.utils.helpers import data_loader
+
+MODEL = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+embedding_matrix_p = pickle.load("data/embedding.pkl")
+df_PICKLE_FILE = pickle.load("data/df.pkl")
+
+def get_similar_responses(question: str, top_k = 5) -> list:
     # TODO: Implement the logic to get the similar responses
-    return ["These are test responses"]
+    data_loader()
+    embedding_matrix = pickle.load(embedding_matrix_p)
+    df = pickle.load(df_PICKLE_FILE)
+    question_encoding = MODEL.encode(question)
+    similarity = cosine_similarity([question_encoding], embedding_matrix)
+    top_k_responses = similarity.argsort()[top_k:]
+    return df.iloc[top_k_responses]['wikipedia_excerpt'].to_list()
